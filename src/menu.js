e4650878361704201f9c12b9fbddfa9566400243
Haris Kovac <hariskovac19@gmail.com>
2023-08-03 19:13:02 -0500

    Add menu items

---
+++ src/menu.js
@@ -0,0 +1,42 @@
+const menuItems = [
+  {
+    name: 'Blackpepper Bacon',
+    price: '7 CP',
+    description: 'A pound of fresh, thick-cut bacon roasted and lightly brushed with our homemade mix of brown sugar, black pepper, garlic, and orange juice. A perfect appetizer for the most daring of adventurers!',
+    image: '../src/blackpepper bacon.png',
+  },
+  {
+    name: 'Adventurer\'s Rations',
+    price: '9 CP',
+    description: 'Packed full of protein and essential nutrients, our rations include an assortment of nuts, fresh cheeses, fruit, and dried meats alongside our homemade rye bread.',
+    image: '../src/adventurer\'s rations.png',
+  },
+  {
+    name: 'Traveller\'s Stew',
+    price: '1 SP',
+    description: 'This hearty stew is slow-cooked over an open flame, chock-full of potatoes, carrots, yellow onions, and a pound of beer-braised beef. A Guild favorite!',
+    image: '../src/traveller\'s stew.png',
+  },
+  {
+    name: 'Honey Glazed Ribs',
+    price: '2 SP',
+    description: 'Your choice of beef or pork ribs slathered with our signature honey glaze, delicately roasted to ensure an even char throughout. Fall-off-the-bone deliciousness!',
+    image: '../src/honey glazed ribs.png',
+  },
+  {
+    name: 'Seafood Bouillabaisse',
+    price: '2 SP',
+    description: 'Fresh-caught haddock, mussels, and clams slowly simmered in a flavorful broth of fish and tomato stock, leeks, chopped garlic, red pepper flakes, and saffron. You\'ll wish you had a ladle for this one.',
+    image: '../src/seafood buillabaisse.png',
+  },
+  {
+    name: 'Apple Blackberry Pie',
+    price: '8 CP',
+    description: 'A flaky pie crust filled with a delicious mixture of crisp apples, plump blackberries, and a dash of ground cinnamon. Served with a delicious whipped sweet cream.',
+    image: '../src/apple blackberry pie.png',
+  },
+]
+
+function loadMenu() {
+  
+}
